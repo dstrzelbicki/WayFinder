af5c14b09f4dc041109324069fb954e7109e93b7
wilkosz <wilkosz1998@gmail.com>
2023-04-10 11:27:35 +0200

    webpack configuration

--- WayFinder/frontend/webpack.config.js
+++ WayFinder/frontend/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].js",
+    filename: "main.js",
   },
   module: {
     rules: [
@@ -21,12 +22,21 @@ module.exports = {
   optimization: {
     minimize: true,
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "static"),
+    },
+    port: 3000,
+  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
         // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("production"),
+        NODE_ENV: JSON.stringify("development"),
       },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
 };
