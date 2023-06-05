505fde619f4a27b19608285140bafeed63e3606b
fms <fms@kmdpoland.pl>
2023-06-05 11:32:52 +0200

    change openAPI , add geolocation markers, upgrade webpack version

--- WayFinder/frontend/src/components/HomePage/HomePage.js
+++ WayFinder/frontend/src/components/HomePage/HomePage.js
@@ -1,125 +1,118 @@
-import React, {useState, useEffect} from "react"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom"
+import React, {useState} from "react"
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom"
 import OLMap from "../Map/Map"
 import SearchBar from "../SearchBar/SearchBar"
 import {geocode} from "../../services/mapServices"
 import Sidebar from "../Sidebar/Sidebar"
 import "./HomePage.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faTimes, faArrowCircleRight} from "@fortawesome/free-solid-svg-icons"
-import LoginPage from "../LoginPage/LoginPage"
-import RegisterPage from "../RegisterPage/RegisterPage";
-import ForgottenPassword from "../ForgottenPasswordPage/ForgottenPassword";
+import {faArrowCircleRight, faTimes} from "@fortawesome/free-solid-svg-icons"
 
 const HomePage = () => {
-  const [marker1, setMarker1] = useState(null)
-  const [marker2, setMarker2] = useState(null)
-  const [marker2Name, setMarker2Name] = useState("")
-  const [isSidebarNotCollapsed, setIsSidebarNotCollapsed] = useState(false)
-  const [showSearchHistory, setShowSearchHistory] = useState(false)
+    const [marker1, setMarker1] = useState(null)
+    const [marker2, setMarker2] = useState(null)
+    const [marker2Name, setMarker2Name] = useState("")
+    const [isSidebarNotCollapsed, setIsSidebarNotCollapsed] = useState(false)
+    const [showSearchHistory, setShowSearchHistory] = useState(false)
 
-  const toggleSidebar = () => {
-    setIsSidebarNotCollapsed(!isSidebarNotCollapsed)
-  }
-
-  const handleSearch = async (searchTerm, searchBarId) => {
-    const data = await geocode(searchTerm)
-    if (data && data.length > 0) {
-      const { lat, lon } = data[0]
-      const coordinates = [parseFloat(lon), parseFloat(lat)]
-      console.log(
-        `SearchBar${searchBarId}: `,
-        searchTerm,
-        "Coordinates: ",
-        lat,
-        lon
-      )
-      if (searchBarId === 1) {
-        setMarker1(coordinates)
-      } else {
-        setMarker2(coordinates)
-      }
-
-      // save search term to local storage
-      let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []
-      searchHistory.push({searchTerm, searchBarId, timestamp: new Date()})
-      localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
-    } else {
-      console.error(
-        `No results found for SearchBar${searchBarId}: `,
-        searchTerm
-      )
+    const toggleSidebar = () => {
+        setIsSidebarNotCollapsed(!isSidebarNotCollapsed)
     }
-  }
 
-  const updateMarker2Name = (name) => {
-    setMarker2Name(name)
-  }
+    const handleSearch = async (searchTerm, searchBarId) => {
+        // fixme add validation and display error when not found
+        const data = await geocode(searchTerm)
+        if (data && data.results.length > 0) {
+            const {lat, lon} = data.results[0]
+            const coordinates = [parseFloat(lon), parseFloat(lat)]
+            console.log(
+                `SearchBar${searchBarId}: `,
+                searchTerm,
+                "Coordinates: ",
+                lat,
+                lon
+            )
+            if (searchBarId === 1) {
+                setMarker1(coordinates)
+            } else {
+                setMarker2(coordinates)
+            }
 
-  //Use 'ctrl + /' to display different pages
+            // save search term to local storage
+            let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []
+            searchHistory.push({searchTerm, searchBarId, timestamp: new Date()})
+            localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
+        } else {
+            console.error(
+                `No results found for SearchBar${searchBarId}: `,
+                searchTerm
+            )
+        }
+    }
 
-  return (
-      // <LoginPage></LoginPage>
-      // <RegisterPage></RegisterPage>
-      // <ForgottenPassword></ForgottenPassword>
-      <Router>
-        <div className="full-height-container">
-          <Sidebar
-            isNotCollapsed={isSidebarNotCollapsed}
-            toggleSidebar={toggleSidebar}
-            onSearchHistoryClick={() => {
-              setShowSearchHistory(true)
-              setIsSidebarNotCollapsed(false)
-            }}
-          />
-          <div className="search-container">
-          {showSearchHistory ? (
-          <div className="search-history-container">
-            <div className="search-history-close" onClick={() => setShowSearchHistory(!showSearchHistory)}>
-              <p>Close search history &nbsp;<FontAwesomeIcon icon={faTimes} /></p>
-            </div>
-            <div className="search-history-list">
-              {JSON.parse(localStorage.getItem("searchHistory")) &&
-                JSON.parse(localStorage.getItem("searchHistory")).map((item, index) => (
-                  <div key={index} className="search-history-item" onClick={() => {
-                    setShowSearchHistory(false)
-                    updateMarker2Name(item.searchTerm)
-                    handleSearch(item.searchTerm, 2)}}
-                  >
+    const updateMarker2Name = (name) => {
+        setMarker2Name(name)
+    }
+
+    //Use 'ctrl + /' to display different pages
+
+    return (
+        // <LoginPage></LoginPage>
+        // <RegisterPage></RegisterPage>
+        // <ForgottenPassword></ForgottenPassword>
+        <Router>
+            <div className="full-height-container">
+                <Sidebar
+                    isNotCollapsed={isSidebarNotCollapsed}
+                    toggleSidebar={toggleSidebar}
+                    onSearchHistoryClick={() => {
+                        setShowSearchHistory(true)
+                        setIsSidebarNotCollapsed(false)
+                    }}
+                />
+                <div className="search-container">
+                    {showSearchHistory ? (
+                        <div className="search-history-container">
+                            <div className="search-history-close" onClick={() => setShowSearchHistory(!showSearchHistory)}>
+                                <p>Close search history &nbsp;<FontAwesomeIcon icon={faTimes}/></p>
+                            </div>
+                            <div className="search-history-list">
+                                {JSON.parse(localStorage.getItem("searchHistory")) &&
+                                    JSON.parse(localStorage.getItem("searchHistory")).map((item, index) => (
+                                        <div key={index} className="search-history-item" onClick={() => {
+                                            setShowSearchHistory(false)
+                                            updateMarker2Name(item.searchTerm)
+                                            handleSearch(item.searchTerm, 2)
+                                        }}
+                                        >
                     <span>
-                      {item.searchTerm}&nbsp;&nbsp;<FontAwesomeIcon icon={faArrowCircleRight} />
+                      {item.searchTerm}&nbsp;&nbsp;<FontAwesomeIcon icon={faArrowCircleRight}/>
                     </span>
-                  </div>
-              ))}
+                                        </div>
+                                    ))}
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <SearchBar
+                                placeholder="Search first location"
+                                onSearch={(searchTerm) => handleSearch(searchTerm, 1)}
+                            />
+                            <br/>
+                            <SearchBar
+                                placeholder={marker2Name || "Search second location"}
+                                onSearch={(searchTerm) => handleSearch(searchTerm, 2)}
+                            />
+                        </>
+                    )}
+                </div>
+                <OLMap marker1={marker1} marker2={marker2} onMarker2NameUpdate={updateMarker2Name}/>
             </div>
-          </div>
-          ) : (
-          <>
-            <SearchBar
-              placeholder="Search first location"
-              onSearch={(searchTerm) => handleSearch(searchTerm, 1)}
-            />
-            <br />
-            <SearchBar
-              placeholder={marker2Name || "Search second location"}
-              onSearch={(searchTerm) => handleSearch(searchTerm, 2)}
-            />
-          </>
-          )}
-          </div>
-          <OLMap marker1={marker1} marker2={marker2} onMarker2NameUpdate={updateMarker2Name} />
-        </div>
-        <Routes>
-          <Route exact path="/" />
-        </Routes>
-      </Router>
-  )
+            <Routes>
+                <Route exact path="/"/>
+            </Routes>
+        </Router>
+    )
 }
 
 export default HomePage
--- WayFinder/frontend/src/services/mapServices.js
+++ WayFinder/frontend/src/services/mapServices.js
@@ -1,47 +1,56 @@
 import axios from "axios"
 import {fromLonLat} from "ol/proj"
 
-const nominatimBaseUrl = "https://nominatim.openstreetmap.org/search"
+const API_KEY = process.env.REACT_APP_GEOAPIFY_API_KEY
+const nominatimBaseUrl = "https://api.geoapify.com/v1/geocode/search"
 
 export const geocode = async (searchTerm) => {
-  const url = `${nominatimBaseUrl}?format=json&q=${encodeURIComponent(searchTerm)}&limit=1`
+    const url = `${nominatimBaseUrl}?text=${encodeURIComponent(searchTerm)}&format=json&apiKey=${API_KEY}`
 
-  try {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error("Error fetching geocoding data:", error)
-    return null
-  }
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    try {
+        const response = await fetch(url, requestOptions)
+        if (response.status >= 400 && response.status < 600) {
+            // fixme - improve error handling
+            console.error("Error fetching geocoding data. Bad response from server: ", response)
+        } else {
+            console.log("Fetched data location:", response)
+        }
+        return await response.json()
+    } catch (error) {
+        console.error("Error fetching geocoding data:", error)
+        return null
+    }
 }
 
-const API_KEY = process.env.REACT_APP_OPENROUTESERVICE_API_KEY
-
+// implement
 export const getRoute = async (start, end) => {
-  const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${API_KEY}&start=${start.join(
-    ","
-  )}&end=${end.join(",")}`
+    const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${API_KEY}&start=${start.join(
+        ","
+    )}&end=${end.join(",")}`
 
-  try {
-    const response = await axios.get(url)
-    const route = response.data.features[0].geometry.coordinates
-    return route.map((coord) => fromLonLat(coord))
-  } catch (error) {
-    console.error("Error fetching route: ", error)
-    return null
-  }
+    try {
+        const response = await axios.get(url)
+        const route = response.data.features[0].geometry.coordinates
+        return route.map((coord) => fromLonLat(coord))
+    } catch (error) {
+        console.error("Error fetching route: ", error)
+        return null
+    }
 }
 
 export const reverseGeocode = async (coordinates) => {
-  const [lon, lat] = coordinates
-  const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`
+    const [lon, lat] = coordinates
+    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`
 
-  try {
-    const response = await axios.get(url)
-    return response.data
-  } catch (error) {
-    console.error("Error reverse geocoding: ", error)
-    return null
-  }
+    try {
+        const response = await axios.get(url)
+        return response.data
+    } catch (error) {
+        console.error("Error reverse geocoding: ", error)
+        return null
+    }
 }
