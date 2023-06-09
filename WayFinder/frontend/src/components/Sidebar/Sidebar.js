4c1e76a7aa252b8bb074c0f76fec610d1676301d
rukkje <darek.czer333@gmail.com>
2023-06-09 16:35:27 +0200

    handle logout

--- WayFinder/frontend/src/components/Sidebar/Sidebar.js
+++ WayFinder/frontend/src/components/Sidebar/Sidebar.js
@@ -21,32 +21,39 @@ const Sidebar = ({ isNotCollapsed, toggleSidebar, onSearchHistoryClick }) => {
 
   return (
     <div className={`sidebar${isNotCollapsed ? " active" : ""}`}>
-        <div className="toggle-button" onClick={toggleSidebar} role="button" aria-label="toggle sidebar">
-          <FontAwesomeIcon icon={isNotCollapsed ? faTimes: faBars} />
-        </div>
-        <h2>WayFinder</h2>
-        <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`}
-          onClick={onSearchHistoryClick}>
-          <p>Search history</p>
-        </div>
-        <div className={`search-history-icon${isNotCollapsed ? " none" : " active"}`}
-          onClick={onSearchHistoryClick} role="button" aria-label="history icon">
-          <FontAwesomeIcon icon={faHistory} />
-        </div>
-        <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`}>
-          <Link to={"/user-dashboard/profile"} style={{textDecoration: "none", color: "inherit"}}>User dashboard</Link>
-        </div>
-        <div className={`user-icon${isNotCollapsed ? " none" : " active"}`}>
-          <Link to={"/user-dashboard/profile"} style={{textDecoration: "none", color: "inherit"}}><FontAwesomeIcon icon={faUserAlt} /></Link>
-        </div>
-        <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`}>
-          <Link to={"/logout"} style={{textDecoration: "none", color: "inherit"}}>Logout</Link>
-        </div>
-        <div className={`logout-icon${isNotCollapsed ? " none" : " active"}`}>
-          <Link to={"/logout"} style={{textDecoration: "none", color: "inherit"}}><FontAwesomeIcon icon={faSignOut} /></Link>
-        </div>
+      <div className="toggle-button" onClick={toggleSidebar} role="button" aria-label="toggle sidebar">
+        <FontAwesomeIcon icon={isNotCollapsed ? faTimes : faBars} />
+      </div>
+      <h2>WayFinder</h2>
+      <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`} onClick={onSearchHistoryClick}>
+        <p>Search history</p>
+      </div>
+      <div className={`search-history-icon${isNotCollapsed ? " none" : " active"}`} onClick={onSearchHistoryClick} role="button" aria-label="history icon">
+        <FontAwesomeIcon icon={faHistory} />
+      </div>
+      <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`}>
+        <Link to={"user-dashboard/profile"} style={{ textDecoration: "none", color: "inherit" }}>
+          User dashboard
+        </Link>
+      </div>
+      <div className={`user-icon${isNotCollapsed ? " none" : " active"}`}>
+        <Link to={"user-dashboard/profile"} style={{ textDecoration: "none", color: "inherit" }}>
+          <FontAwesomeIcon icon={faUserAlt} />
+        </Link>
+      </div>
+      <div className={`sidebar-text${isNotCollapsed ? " active" : " none"}`}>
+        <button onClick={handleLogout} style={{ textDecoration: "none", color: "inherit", background: "none", border: "none", cursor: "pointer" }}>
+          Logout
+        </button>
+      </div>
+      <div className={`logout-icon${isNotCollapsed ? " none" : " active"}`}>
+        <button onClick={handleLogout} style={{ textDecoration: "none", color: "inherit", background: "none", border: "none", cursor: "pointer" }}>
+          <FontAwesomeIcon icon={faSignOut} />
+        </button>
+      </div>
     </div>
   );
 };
+
 
 export default Sidebar;
