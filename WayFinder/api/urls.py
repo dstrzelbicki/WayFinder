85e877406e05e0492f78b03d84ecd74d912f241a
dstrzelbicki <dstrzelbicki@gmail.com>
2023-06-10 06:00:48 +0200

    password reset and reset link mail added

--- WayFinder/api/urls.py
+++ WayFinder/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', views.UserRegister.as_view(), name='register'),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('user', views.UserView.as_view(), name='user'),
     path('change-password', views.UserChangePassword.as_view(), name='change_password'),
     path('route', views.RouteView.as_view(), name='route'),
+
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
