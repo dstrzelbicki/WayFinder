0fea28ef7417cc6e8e75c2fb913fbc39fd6e8380
dstrzelbicki <dstrzelbicki@gmail.com>
2023-06-17 18:54:20 +0200

    reset password improved

--- WayFinder/api/urls.py
+++ WayFinder/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('register', views.UserRegister.as_view(), name='register'),
@@ -10,8 +11,8 @@ urlpatterns = [
     path('change-password', views.UserChangePassword.as_view(), name='change_password'),
     path('route', views.RouteView.as_view(), name='route'),
 
-    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- WayFinder/api/views.py
+++ WayFinder/api/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
@@ -220,6 +222,6 @@ class ResetPasswordConfirmView(APIView):
             # Reset the user's password
             user.set_password(password)
             user.save()
-            return JsonResponse({'message': 'Password reset successful'})
+            return redirect('login')
         else:
-            return JsonResponse({'message': 'Invalid or expired token'}, status=400)
+            return HttpResponse('Invalid or expired token', status=400)
