6b00054fca1a0565bffece11e68c8a1975314f85
fms <fms@kmdpoland.pl>
2023-11-08 20:57:26 +0100

    refactor Homepage css in progress - not fully working

--- WayFinder/frontend/src/components/HomePage/HomePage.js
+++ WayFinder/frontend/src/components/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react"
-import OLMap from "../Map/Map"
 import SearchBox from "../SearchBox/SearchBox.js"
 import {geocode} from "../../services/mapServices"
 import Sidebar from "../Sidebar/Sidebar"
@@ -8,6 +7,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faArrowCircleRight, faMinus, faPlus, faTimes} from "@fortawesome/free-solid-svg-icons"
 import TransportOptions from "./TransportOptions/TransportOptions";
 import {Typography} from "@mui/material";
+import OLMap from "../Map/Map";
 
 const HomePage = () => {
     const [marker1, setMarker1] = useState(null)
@@ -87,7 +87,7 @@ const HomePage = () => {
 
     const handleOptionChange2 = (option) => {
         if (selectedOption2 === option) {
-            setSelectedOption1('');
+            setSelectedOption2('');
         } else {
             setSelectedOption2(option);
         }
@@ -103,7 +103,6 @@ const HomePage = () => {
     return (
 
         <div className="full-height-container">
-
             <Sidebar
                 isNotCollapsed={isSidebarNotCollapsed}
                 toggleSidebar={toggleSidebar}
@@ -112,59 +111,49 @@ const HomePage = () => {
                     setIsSidebarNotCollapsed(false)
                 }}
             />
-
-
-            {showSearchHistory ? (
-                <div className="search-history-container">
-                    <div className="search-history-close" onClick={() => setShowSearchHistory(!showSearchHistory)}>
-                        <p>Close search history &nbsp;<FontAwesomeIcon icon={faTimes}/></p>
-                    </div>
-                    <div className="search-history-list">
-                        {JSON.parse(localStorage.getItem("searchHistory")) &&
-                            JSON.parse(localStorage.getItem("searchHistory")).map((item, index) => (
-                                <div key={index} className="search-history-item" onClick={() => {
-                                    setShowSearchHistory(false)
-                                    updateMarker2Name(item.searchTerm)
-                                    handleSearch(item.searchTerm, 2)
-                                }}
-                                >
+            <div className="search-container">
+                {showSearchHistory ? (
+                    <div className="search-history-container">
+                        <div className="search-history-close" onClick={() => setShowSearchHistory(!showSearchHistory)}>
+                            <p>Close search history &nbsp;<FontAwesomeIcon icon={faTimes}/></p>
+                        </div>
+                        <div className="search-history-list">
+                            {JSON.parse(localStorage.getItem("searchHistory")) &&
+                                JSON.parse(localStorage.getItem("searchHistory")).map((item, index) => (
+                                    <div key={index} className="search-history-item" onClick={() => {
+                                        setShowSearchHistory(false)
+                                        updateMarker2Name(item.searchTerm)
+                                        handleSearch(item.searchTerm, 2)
+                                    }}
+                                    >
                     <span>
                       {item.searchTerm}&nbsp;&nbsp;<FontAwesomeIcon icon={faArrowCircleRight}/>
                     </span>
-                                </div>
-                            ))}
+                                    </div>
+                                ))}
+                        </div>
                     </div>
-                </div>
-            ) : (
-                <div className="search-location-container">
-                    <div className="search-box-container1">
+                ) : (
+                    <div className="search-location-container">
                         <SearchBox placeholder="Search first location" onSearch={(searchTerm) => handleSearch(searchTerm, 1)}/>
-                        <button className="add-stop-button" onClick={handleAddStop}><FontAwesomeIcon icon={showAddStop ? faMinus : faPlus}/></button>
-                    </div>
-
-                    <div className="search-box-container2">
                         <SearchBox placeholder={marker2Name || "Search second location"} onSearch={(searchTerm) => handleSearch(searchTerm, 2)}/>
+                        <div className="add-stop-button">
+                            <button className="add-stop-button" onClick={handleAddStop}><FontAwesomeIcon icon={showAddStop ? faMinus : faPlus}/></button>
+                        </div>
+                        {showAddStop && (
+                            <>
+                                <TransportOptions selectedOption={selectedOption2} handleOptionChange={handleOptionChange2}/>
+                                <div className="search-box-container">
+                                    <SearchBox placeholder="Add stop" onSearch={(searchTerm) => handleSearch(searchTerm, 3)} grayText/>
+                                </div>
+                            </>
+                        )}
+
+                        <TransportOptions selectedOption={selectedOption1} handleOptionChange={handleOptionChange}/>
+                        <Typography variant="h2">Selected Options: {selectedOption1}</Typography>
                     </div>
-
-                    {showAddStop && (
-                        <>
-                            <TransportOptions selectedOption={selectedOption1} handleOptionChange={handleOptionChange}/>
-
-                            <div className="search-box-container1">
-                                <SearchBox placeholder="Add stop" onSearch={(searchTerm) => handleSearch(searchTerm, 3)} grayText/>
-                            </div>
-
-                            <TransportOptions selectedOption={selectedOption2} handleOptionChange={handleOptionChange2}/>
-                        </>
-                    )}
-
-                    {!showAddStop && (<TransportOptions selectedOption={selectedOption1} handleOptionChange={handleOptionChange}/>)}
-
-                    <Typography variant="subtitle2">Selected Options: {selectedOption1}</Typography>
-
-                </div>
-            )}
-
+                )}
+            </div>
             <OLMap marker1={marker1} marker2={marker2} marker3={marker3} transportOption1={selectedOption1} transportOption2={selectedOption2} onMarker2NameUpdate={updateMarker2Name} isPlusIcon={!isMinusIcon}/>
 
         </div>
--- WayFinder/frontend/src/components/HomePage/TransportOptions/TransportOptions.js
+++ WayFinder/frontend/src/components/HomePage/TransportOptions/TransportOptions.js
@@ -6,8 +6,8 @@ import "./TransportOptions.css"
 function TransportOptions(props) {
 
     const transportOptions = [
-        {label: "Car", value: "Car", icon: <DriveEta/>},
-        {label: "Bicycle", value: "Bicycle", icon: <DirectionsBike/>},
+        {label: "Car", value: "Car", icon: <DriveEta style={{height: 20,  width: 17}}/>},
+        {label: "Bicycle", value: "Bicycle", icon: <DirectionsBike style={{height: 20,  width: 17}}/>},
         // Add more options as needed
     ]
 
@@ -16,25 +16,25 @@ function TransportOptions(props) {
     };
 
     return (<div className="transport-options-container">
-            <Typography variant="subtitle1">Select Transport Options:</Typography>
+            <Typography variant="h2">Select Transport Options:</Typography>
             <FormGroup>
                 {transportOptions.map((option) => (
                     <FormControlLabel
-                        key={option.value}
-                        control={
-                            <Checkbox
-                                checked={props.selectedOption === option.value}
-                                onChange={() => handleOptionChange(option.value)}
-                                color="primary"
-                                disabled={props.selectedOption && props.selectedOption !== option.value}
-                            />
-                        }
-                        label={
-                            <div>
-                                <ListItemIcon>{option.icon}</ListItemIcon>
-                                <span>{option.label}</span>
-                            </div>
-                        }
+                                      key={option.value}
+                                      control={
+                                          <Checkbox size='small'
+                                              checked={props.selectedOption === option.value}
+                                              onChange={() => handleOptionChange(option.value)}
+                                              color="secondary"
+                                              disabled={props.selectedOption && props.selectedOption !== option.value}
+                                          />
+                                      }
+                                      label={
+                                          <div className="transport-options-label">
+                                              <ListItemIcon style={{minWidth: '25px'}}>{option.icon}</ListItemIcon>
+                                              <span style={{fontSize:9}}>{option.label}</span>
+                                          </div>
+                                      }
                     />
                 ))}
             </FormGroup>
