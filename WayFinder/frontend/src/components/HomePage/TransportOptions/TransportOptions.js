31af748d1a9e4be833f4c6dcf4026b6cb1f590a4
fms <fms@kmdpoland.pl>
2023-11-09 20:01:17 +0100

    change TransportOptions styling

--- WayFinder/frontend/src/components/HomePage/TransportOptions/TransportOptions.js
+++ WayFinder/frontend/src/components/HomePage/TransportOptions/TransportOptions.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Checkbox, FormControlLabel, FormGroup, ListItemIcon, Typography} from "@material-ui/core";
 import {DirectionsBike, DriveEta} from "@material-ui/icons";
 import "./TransportOptions.css"
+import {ListItem, ListItemText} from "@mui/material";
 
 function TransportOptions(props) {
 
     const transportOptions = [
-        {label: "Car", value: "Car", icon: <DriveEta style={{height: 20,  width: 17}}/>},
-        {label: "Bicycle", value: "Bicycle", icon: <DirectionsBike style={{height: 20,  width: 17}}/>},
+        {label: "Car", value: "Car", icon: <DriveEta style={{height: 20, width: 17}}/>},
+        {label: "Bicycle", value: "Bicycle", icon: <DirectionsBike style={{height: 20, width: 17}}/>},
         // Add more options as needed
     ]
 
@@ -20,21 +21,24 @@ function TransportOptions(props) {
             <FormGroup>
                 {transportOptions.map((option) => (
                     <FormControlLabel
-                                      key={option.value}
-                                      control={
-                                          <Checkbox size='small'
-                                              checked={props.selectedOption === option.value}
-                                              onChange={() => handleOptionChange(option.value)}
-                                              color="secondary"
-                                              disabled={props.selectedOption && props.selectedOption !== option.value}
-                                          />
-                                      }
-                                      label={
-                                          <div className="transport-options-label">
-                                              <ListItemIcon style={{minWidth: '25px'}}>{option.icon}</ListItemIcon>
-                                              <span style={{fontSize:9}}>{option.label}</span>
-                                          </div>
-                                      }
+                        key={option.value}
+                        control={
+                            <Checkbox size='small'
+                                      checked={props.selectedOption === option.value}
+                                      onChange={() => handleOptionChange(option.value)}
+                                      color="default"
+                                      disabled={props.selectedOption && props.selectedOption !== option.value}
+                            />
+                        }
+                        label={
+                            <ListItem>
+                                <ListItemIcon style={{minWidth: '25px'}}>{option.icon}</ListItemIcon>
+                                <ListItemText
+                                    disableTypography
+                                    primary={<Typography style={{ fontSize:9, color: '#424242' }}>{option.label}</Typography>}
+                                />
+                            </ListItem>
+                        }
                     />
                 ))}
             </FormGroup>
